9a1f59b7d0b5f38fb82c6544e4df9b223e0b6b56
Kurtis Holsapple <kurtis@openfunctioncomputers.com>
2025-02-12 13:50:45 -0800

    redundant types

--- script-runner/envScripts.go
+++ script-runner/envScripts.go
@@ -1,36 +1,36 @@
 package scriptrunner
 
 var envScripts = map[string]envScript{
-	"export": envScript{
+	"export": {
 		filename:     "export-account.sh",
 		requiredEnvs: []string{"ACCOUNT"},
 	},
-	"import": envScript{
+	"import": {
 		filename:     "ofco-restore-site-from-backup.sh",
 		requiredEnvs: []string{"BACKUP_TARBALL", "DESTINATION_ACCOUNT"},
 	},
-	"addAccount": envScript{
+	"addAccount": {
 		filename:     "create-new-account.sh",
 		requiredEnvs: []string{"ACCOUNT_NAME"},
 		optionalEnvs: []string{"DOMAIN"},
 	},
-	"cloneAccount": envScript{
+	"cloneAccount": {
 		filename:     "clone-wordpress-account.sh",
 		requiredEnvs: []string{"SOURCE_ACCOUNT", "DESTINATION_ACCOUNT", "DESTINATION_DOMAIN"},
 	},
-	"terminateAccount": envScript{
+	"terminateAccount": {
 		filename:     "ofco-delete-account.sh",
 		requiredEnvs: []string{"ACCOUNT_TO_DELETE"},
 	},
-	"addDomainToAccount": envScript{
+	"addDomainToAccount": {
 		filename:     "add-domain-to-account.sh",
 		requiredEnvs: []string{"ACCOUNT", "DOMAIN"},
 	},
-	"unban-ip": envScript{
+	"unban-ip": {
 		filename:     "unban-ip.sh",
 		requiredEnvs: []string{"IPADDRESS"},
 	},
-	"update-wordpress-domain": envScript{
+	"update-wordpress-domain": {
 		filename:     "change-wordpress-domain.sh",
 		requiredEnvs: []string{"DOMAIN", "ACCOUNT"},
 	},
